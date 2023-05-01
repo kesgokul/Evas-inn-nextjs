dad75b1fad920ec9b3907c3414964409ea4a28de
kesgokul <gokulkesavan5005@gmail.com>
2023-05-01 16:07:31 +0530

    hero staggered animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,23 +1,18 @@
+import HeroPage from "./HeroPage";
+import { heroData } from "@/utils/hero-data";
+
 export default function Hero() {
   return (
-    <main className="px-2 md:px-12 min-h-screen max-w-full flex flex-wrap justify-between items-center bg-dog-1 bg-cover bg-right-bottom">
-      <div className=" w-full max-w-md shrink-0  flex flex-col gap-4">
-        <h1 className="text-yellow-500 text-4xl md:text-6xl font-bold uppercase tracking-wider">
-          dog
-        </h1>
-        <h2 className="text-white font-mono text-xl md:text-2xl">
-          Boarding facility
-        </h2>
-        <p className="text-white tracking-wide">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur
-          vitae reprehenderit deserunt eligendi dolor accusamus assumenda
-          placeat repellendus ab optio!
-        </p>
-        <button className=" py-3 px-10 bg-gray-50 text-gray-600 self-start">
-          Board now
-        </button>
-      </div>
-      {/* <figure className=" grow h-2/3-screen  mx-auto basis-96 shrink-0 bg-nemo-black bg-contain bg-black bg-blend-darken bg-opacity-30    "></figure> */}
-    </main>
+    <>
+      {heroData.map((data, i) => (
+        <HeroPage
+          key={i}
+          title={data.title}
+          sub={data.sub}
+          desc={data.desc}
+          img={data.img}
+        />
+      ))}
+    </>
   );
 }
---
+++ src/components/HeroPage.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { delay, motion } from "framer-motion";
+import { useState } from "react";
+import { heroDataType } from "@/utils/hero-data";
+
+const children = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      ease: "easeIn",
+      duration: 0.5,
+    },
+  },
+};
+
+export default function HeroPage({ title, sub, desc, img }: heroDataType) {
+  const [staggerCount, setStaggerCount] = useState<number>(0);
+
+  function increment(): void {
+    setStaggerCount((s) => s++);
+  }
+  return (
+    <main
+      className={`px-2 md:px-12 min-h-screen max-w-full flex flex-wrap justify-between items-center bg-${img} bg-cover bg-center lg:bg-right-bottom snap-start shrink-0`}
+    >
+      <motion.div
+        initial={"hidden"}
+        whileInView={"show"}
+        transition={{ staggerChildren: 0.5 }}
+        className=" w-full max-w-md shrink-0  flex flex-col gap-4"
+      >
+        <motion.h1
+          variants={children}
+          className="text-yellow-500 text-4xl md:text-6xl font-bold uppercase tracking-wider"
+        >
+          {title}
+        </motion.h1>
+        <motion.h2
+          variants={children}
+          className="text-white font-mono text-xl md:text-2xl"
+        >
+          {sub}
+        </motion.h2>
+        <motion.p variants={children} className="text-white tracking-wide">
+          {desc}
+        </motion.p>
+        <motion.button
+          variants={children}
+          className=" py-3 px-10 bg-gray-50 text-gray-600 self-start"
+        >
+          Board now
+        </motion.button>
+      </motion.div>
+      {/* <figure className=" grow h-2/3-screen  mx-auto basis-96 shrink-0 bg-nemo-black bg-contain bg-black bg-blend-darken bg-opacity-30    "></figure> */}
+    </main>
+  );
+}
